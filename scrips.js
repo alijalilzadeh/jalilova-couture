ae96e959a4d4e9239c7974445f77031196fb9628
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-09-23 21:05:17 +0400

    I added Dresses boxes on it

--- scrips.js
+++ scrips.js
@@ -1,11 +1,11 @@
   const phone = "994553924931"; 
-  const as = document.querySelectorAll("a");
+  const as = document.querySelectorAll(".orderLinks");
 
  as.forEach(a => {
     a.addEventListener("click", () => {
       const product = a.dataset.product;
       const imgUrl = a.dataset.img;
-      const message = `Mən bu məhsulu sifariş etmək istəyirəm:\n${product}\n${imgUrl}`;
+      const message = `Salam, Mən bu məhsulu sifariş etmək istəyirəm:\n${product}\n${imgUrl}`;
       const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
       window.open(url, "_blank");
     });
