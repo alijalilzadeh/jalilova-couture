ab31973831229f449585960a4ff79bc971534bbe
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-10-05 18:07:45 +0400

    Feat:Little bugs fixed

--- scrips.js
+++ scrips.js
@@ -1,4 +1,4 @@
-  const phoneNumberNumber = "994556028288"; 
+  const phoneNumber = "994556028288"; 
   const as = document.querySelectorAll(".orderLinks");
 
  as.forEach(a => {
@@ -30,7 +30,8 @@
 
 const personalOrder = document.querySelector(".personal-order");
 personalOrder.addEventListener("click",()=>{
-  const message = "Salam, Jalilova Couture saytından yazıram. Özümə xüsusi geyim tikdirmək istəyirəm."
-  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-  window.open(url,"_blank")
+  const message2 = "Salam, Jalilova Couture saytından yazıram. Özümə xüsusi geyim tikdirmək istəyirəm."
+  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message2)}`;
+  window.open(url, "_blank");
+
 })
