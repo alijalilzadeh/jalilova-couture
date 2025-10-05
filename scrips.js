07e20eda948c1d501b9062d2776622985124c617
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-10-05 19:08:47 +0400

    Feat:Some service condiitons added

--- scrips.js
+++ scrips.js
@@ -1,37 +1,49 @@
-  const phoneNumber = "994556028288"; 
-  const as = document.querySelectorAll(".orderLinks");
+const phoneNumber = "994556028288";
+const as = document.querySelectorAll(".orderLinks");
 
- as.forEach(a => {
-    a.addEventListener("click", () => {
-      const product = a.dataset.product;
-      const imgUrl = a.dataset.img;
-      const message = `Salam, Mən bu geyimi sifariş etmək istəyirəm:${product}\n${imgUrl}`;
-      const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-      window.open(url, "_blank");
-    });
+as.forEach(a => {
+  a.addEventListener("click", () => {
+    const product = a.dataset.product;
+    const imgUrl = a.dataset.img;
+    const message = `Salam, Mən bu geyimi sifariş etmək istəyirəm:${product}\n${imgUrl}`;
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
   });
+});
 
-  const observer = new IntersectionObserver(entries=>{
-    entries.forEach(entry =>{
-      if(entry.isIntersecting){
-        entry.target.classList.add("interect")
-      }
-      else{
-        entry.target.classList.remove("interect")
-      }
-    })
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("interect")
+    }
+    else {
+      entry.target.classList.remove("interect")
+    }
   })
+})
 
-  const reviewBoxes = document.querySelectorAll(".reviews-box");
+const reviewBoxes = document.querySelectorAll(".reviews-box");
 
-  reviewBoxes.forEach(reviewBox =>{
-    observer.observe(reviewBox);
-  })
+reviewBoxes.forEach(reviewBox => {
+  observer.observe(reviewBox);
+})
 
 const personalOrder = document.querySelector(".personal-order");
-personalOrder.addEventListener("click",()=>{
+personalOrder.addEventListener("click", () => {
   const message2 = "Salam, Jalilova Couture saytından yazıram. Özümə xüsusi geyim tikdirmək istəyirəm."
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message2)}`;
   window.open(url, "_blank");
+});
 
-})
+const termsBox = document.querySelector(".terms-box");
+const arrowIcon = document.querySelector(".terms-service i");
+document.querySelector(".order-form-box .terms-service").addEventListener("click", () => {
+  termsBox.classList.toggle("active");
+  if (termsBox.style.display === "none" || !termsBox.style.display) {
+    arrowIcon.className = "fa-solid fa-arrow-up";
+    termsBox.style.display = "flex";
+  } else {
+    arrowIcon.className = "fa-solid fa-arrow-down";
+    termsBox.style.display = "none";
+  }
+});
